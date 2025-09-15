de0e1f771b6e9cbcb1a1a866ba17f276b20e1e9c
EmilFernandez-29 <emil_fernandez09@outlook.com>
2025-09-15 17:23:14 -0300

    Nueva rama

--- Eventos.js
+++ Eventos.js
@@ -1,4 +1,11 @@
+const boton= document.getElementById("boton");
 const div= document.getElementById("miDiv");
+
+boton.addEventListener("click",function(boton){
+    boton.stopPropagation();
+    alert("Hola soy el botón!");
+});
+
 div.addEventListener("click", function() {
     alert("Hola! Soy el div");
 });
